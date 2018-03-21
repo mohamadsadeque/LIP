35457f023580d2eb55b5c4392ff6f31d499ea962
mohamadsadeque <30416594+mohamadsadeque@users.noreply.github.com>
2018-03-21 10:31:36 -0300

    Create pot.cpp

---
+++ LIP/lista1/pot.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int pot(int b, int e){
+    int produto = b;
+    for(int i  = 1; i < e; i++ ){
+        produto *= b;
+    }
+    return produto;
+}
+int main()
+{
+    int bi, bf, ei, ef;
+    cin >> bi;
+    cin >> bf;
+    cin >> ei;
+    cin >> ef;
+    for(int i = bi; i <= bf; i++){
+            cout << " " << endl;
+        for(int j = ei; j <= ef; j++){
+            if(j == 0){
+                cout << 1 << " ";
+            }
+            if(j == 1)
+                cout << i << " ";
+            else
+                cout << pot(i, j) << " ";
+        }
+    }
+    return 0;
+}
