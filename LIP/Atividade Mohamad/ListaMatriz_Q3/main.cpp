3c4c7d1ed27e1e6358c0a891a668d06c9ba2982c
mohamadsadeque <30416594+mohamadsadeque@users.noreply.github.com>
2018-06-02 11:28:29 -0300

    Update main.cpp

--- LIP/Atividade Mohamad/ListaMatriz_Q3/main.cpp
+++ LIP/Atividade Mohamad/ListaMatriz_Q3/main.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
-#define TAM 3
+#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
-int A[TAM][TAM]= {
-    {1, 2,4},
-    {3,4,6},
-    {1,2,3}
-};
-int B[TAM][TAM];
-void somaTransposta(int v[TAM][TAM],int soma[TAM][TAM]){
-    for(int i = 0; i < TAM; i++){
-        for(int j = 0; j< TAM; j++){
-            soma[i][j] = v[i][j] + v[j][i];
-        }
-    }
-}
 
 int main()
 {
-    somaTransposta(A,B);
-    for(int i = 0; i < TAM; i++){
-        for(int j = 0; j <TAM ; j++){
-            cout << B[i][j] << " ";
+   srand(time(NULL));
+  int n;
+    cout << "DIGITE A ORDEM DA MATRIZ" << endl;
+    cin >> n;
+    int a[n][n];
+     for(int i = 0; i < n; i++){
+        for(int j = 0; j< n; j++){
+            a[i][j] = 1 + rand()%10;
+        }
+    }
+    int b[n][n];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j< n; j++){
+            b[i][j] = a[i][j] + a[j][i];
+        }
+    }
+    cout << "MATRIZ ORIGINAL" << endl;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <n ; j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "\n \n MATRIZ TRANSPOSTA" << endl;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <n ; j++){
+            cout << a[j][i] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "\n \n SOMA" << endl;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <n ; j++){
+            cout << b[i][j] << " ";
         }
         cout << endl;
     }
